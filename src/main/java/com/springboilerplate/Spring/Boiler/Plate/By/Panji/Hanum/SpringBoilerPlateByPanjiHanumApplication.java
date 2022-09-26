e1a9b587f34a6c8c516b780c6d979800ceaf37ea
panjihanumz <83002940+panjihanumz@users.noreply.github.com>
2022-09-26 13:54:03 +0700

    Spring Intializer

---
+++ src/main/java/com/springboilerplate/Spring/Boiler/Plate/By/Panji/Hanum/SpringBoilerPlateByPanjiHanumApplication.java
@@ -0,0 +1,13 @@
+package com.springboilerplate.Spring.Boiler.Plate.By.Panji.Hanum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoilerPlateByPanjiHanumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoilerPlateByPanjiHanumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboilerplate/Spring/Boiler/Plate/By/Panji/Hanum/SpringBoilerPlateByPanjiHanumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboilerplate.Spring.Boiler.Plate.By.Panji.Hanum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoilerPlateByPanjiHanumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
